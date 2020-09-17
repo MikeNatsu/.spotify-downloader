4344c2d7934a6cfeebbeada26049fd1d6b50b672
Anthony-Zeind <anthonyemma55@gmail.com>
2020-09-17 19:45:24 -0400

    Readme modified

--- descargar_canción.py
+++ descargar_canción.py
@@ -2,7 +2,7 @@ import os
 import time
 
 
-
+if 
 
 
 
@@ -13,20 +13,20 @@ option = (input(" 1. Descargar canción. \n 2. Descargar album. \n 3. Descargar 
 
 if option == "1":
     song = input("\n Ingrese nombre o enlace de canción. (Por defecto busqueda en YouTube): ")
-    os.system("cd /home/anthony/Música && spotdl -s " + song)
+    os.system("cd /home/$USERNAME/Música && spotdl -s " + song)
     time.sleep(2)
     print("\n Descarga terminada.")
 elif option == "2":
     album_name = input("\n Ingrese nombre del album: ")
     album_link = input("\n Ingrese el enlace el album: ")
-    os.system("mkdir /home/anthony/Música/'" + album_name + "'")
+    os.system("mkdir /home/$USERNAME/Música/'" + album_name + "'")
     os.system("cd " + music_location + "'" + album_name + "' && spotdl -a " + album_link)
     os.system("cd " + music_location + "'" + album_name + "' && spotdl -l *.txt")
     os.system("cd " + music_location + "'" + album_name + "' && rm *.txt")
 elif option == "3":
     ps_name = input("\n Ingrese nombre del la lista: ")
     ps_link = input("\n Ingrese el enlace de la lista: ")
-    os.system("mkdir /home/anthony/Música/'" + ps_name + "'")
+    os.system("mkdir /home/$USERMANE/Música/'" + ps_name + "'")
     os.system("cd " + music_location + "'" + ps_name + "' && spotdl -a " + ps_link)
     os.system("cd " + music_location + "'" + ps_name + "' && spotdl -l *.txt")
     os.system("cd " + music_location + "'" + ps_name + "' && rm *.txt")
@@ -38,60 +38,3 @@ else:
 
 input("\n Presiona cualquir tecla para continuar")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Verificación de carpeta, terminar la comprobación
-# import os
-# if os.path.isdir('/home/dev/Documentos/python/archivos_prueba/'):
-#     print('La carpeta existe.');
-# else:
-#     print('La carpeta no existe.');
-# if os.path.isfile('/home/dev/Documentos/python/archivos/archivo.txt'):
-#     print('El archivo existe.');
-# else:
-#     print('El no archivo existe.');
