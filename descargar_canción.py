305d673ab3981e07278f72ca5c74e0ec8bc8f6d3
Anthony-Zeind <anthonyemma55@gmail.com>
2020-09-17 19:51:37 -0400

    Update files *.py

--- descargar_canción.py
+++ descargar_canción.py
@@ -2,14 +2,12 @@ import os
 import time
 
 
-if 
-
 
 
 music_location = "/home/$USERNAME/Música/"
 
 print("\n Spotify - YouTube downloader \n")
-option = (input(" 1. Descargar canción. \n 2. Descargar album. \n 3. Descargar playlist. \n 0. Salir. \n Ingrese el número de la opción deseada: "))
+option = (input(" 1. Descargar canción. \n 2. Descargar álbum. \n 3. Descargar playlist. \n 0. Salir. \n Ingrese el número de la opción deseada: "))
 
 if option == "1":
     song = input("\n Ingrese nombre o enlace de canción. (Por defecto busqueda en YouTube): ")
@@ -17,12 +15,12 @@ if option == "1":
     time.sleep(2)
     print("\n Descarga terminada.")
 elif option == "2":
-    album_name = input("\n Ingrese nombre del album: ")
-    album_link = input("\n Ingrese el enlace el album: ")
-    os.system("mkdir /home/$USERNAME/Música/'" + album_name + "'")
-    os.system("cd " + music_location + "'" + album_name + "' && spotdl -a " + album_link)
-    os.system("cd " + music_location + "'" + album_name + "' && spotdl -l *.txt")
-    os.system("cd " + music_location + "'" + album_name + "' && rm *.txt")
+    álbum_name = input("\n Ingrese nombre del álbum: ")
+    álbum_link = input("\n Ingrese el enlace el álbum: ")
+    os.system("mkdir /home/$USERNAME/Música/'" + álbum_name + "'")
+    os.system("cd " + music_location + "'" + álbum_name + "' && spotdl -a " + álbum_link)
+    os.system("cd " + music_location + "'" + álbum_name + "' && spotdl -l *.txt")
+    os.system("cd " + music_location + "'" + álbum_name + "' && rm *.txt")
 elif option == "3":
     ps_name = input("\n Ingrese nombre del la lista: ")
     ps_link = input("\n Ingrese el enlace de la lista: ")
@@ -36,5 +34,5 @@ else:
     print(" \n Opción invalida")
 
 
-input("\n Presiona cualquir tecla para continuar")
+input.("\n Presiona cualquier tecla para continuar")
 
